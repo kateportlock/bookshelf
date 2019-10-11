a7b509668554d5f9da4a4c702b2cd2968bc459fa
kate-gill <kategill@email.com>
2019-10-11 13:27:19 +0100

    Updates

---
+++ precache-manifest.7bd56ddd1b171c6b186e54fa0762571d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "adab942ed1ad2850b2bdf3a4b8797286",
+    "url": "/bookshelf/index.html"
+  },
+  {
+    "revision": "6bb53ec854b078b29dda",
+    "url": "/bookshelf/static/css/main.4848abe1.chunk.css"
+  },
+  {
+    "revision": "be99ceb866b526c4d8b9",
+    "url": "/bookshelf/static/js/2.5e5000b0.chunk.js"
+  },
+  {
+    "revision": "6bb53ec854b078b29dda",
+    "url": "/bookshelf/static/js/main.3a649c93.chunk.js"
+  },
+  {
+    "revision": "3b4f28f48172c507f7e9",
+    "url": "/bookshelf/static/js/runtime~main.750bc25c.js"
+  },
+  {
+    "revision": "8b4981fa47adfaed1a95b63796f1d124",
+    "url": "/bookshelf/static/media/background.8b4981fa.jpeg"
+  }
+]);
